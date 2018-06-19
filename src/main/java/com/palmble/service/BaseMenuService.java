b13ed7645eb3d5b1ae60bfcf2110ed0536b6a517
wangyanke168 <443666413@qq.com>
2018-06-19 13:43:19 +0800

    增加文件

--- src/main/java/com/palmble/service/BaseMenuService.java
+++ src/main/java/com/palmble/service/BaseMenuService.java
@@ -4,10 +4,17 @@ import java.util.Map;
 
 import com.github.pagehelper.Page;
 import com.palmble.entity.BaseMenu;
+import com.palmble.utils.ResultInfo;
 
 
 public interface BaseMenuService {
 	Page<Object> getMenuList(Map<String, String> map);
 
 	BaseMenu getMenuInfo(Integer menuId);
+
+	ResultInfo deleteMenu(Integer menuId);
+
+	ResultInfo forBiddenMenu(Integer menuId);
+
+	ResultInfo noAvailMenu(Integer menuId);
 }
