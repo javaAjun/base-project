fd470b6e0ea0bdb534c1615476cf91762f0d15b2
wangyanke168 <443666413@qq.com>
2018-07-27 09:10:24 +0800

    增加百度富文本编辑器

---
+++ src/main/java/com/palmble/controller/UEditorController.java
@@ -0,0 +1,34 @@
+package com.palmble.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.palmble.ueditor.ActionEnter;
+
+@RestController
+public class UEditorController {
+
+	
+	@RequestMapping(value="/config")
+    public void config(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("application/json");
+        String rootPath = request.getSession().getServletContext().getRealPath("/");
+        try {
+            String exec = new ActionEnter(request, rootPath).exec();
+            PrintWriter writer = response.getWriter();
+            writer.write(exec);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
+    }
+
+}
---
+++ src/main/java/com/palmble/ueditor/define/ActionState.java
@@ -0,0 +1,5 @@
+package com.palmble.ueditor.define;
+
+public enum ActionState {
+	UNKNOW_ERROR
+}
